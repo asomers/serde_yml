d0c82873612f77828b46c1568a739d16b1625fb9
Alan Somers <asomers@gmail.com>
2024-05-12 10:10:57 -0600

    Eliminate macro_utility
    It's unused, untested, and unnecessary for the library use of the crate.

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -3,7 +3,3 @@ pub mod macro_from_number;
 
 /// The `partialeq_numeric_macros` module contains macros related to the `PartialEq` trait for primitive numeric types and `Value`.
 pub mod macro_partialeq_numeric;
-
-/// The `utility_macros` module contains utility macros for common tasks such as
-/// replacing placeholders in a line with values from parameters.
-pub mod macro_utility;
